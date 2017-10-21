0e87ad2d7eb380b22e6e1c4ae2f33ca34211fc7c
Eduard <edvoinea@gmail.com>
2017-10-21 14:59:54 +0300

    solved old package failure

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,14 @@
-var JwtStrategy = require('passport-jwt').Strategy;
+'use strict'
 
-var User = require('../models/user');
-var config = require('../config/db');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+const config = require('../config/db');
 
 module.exports = function (passport) {
     var opts = {};
     opts.secretOrKey = config.secret;
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
     passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
         User.findOne({id: jwt_payload.id}, function (err, user) {
             if (err) {
