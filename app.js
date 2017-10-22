457908cc5826aaa70cbf72226e197c9ee30f772d
Eduard <edvoinea@gmail.com>
2017-10-22 14:05:01 +0300

    deploy fix

--- app.js
+++ app.js
@@ -9,13 +9,11 @@ const config      = require('./config/db');
 const User        = require('./models/user.js');
 const port        = process.env.PORT || 8080;
 const apiRoutes   = express.Router();
-var BigNumber = require('bignumber.js');
-
-var Web3 = require('web3');
-var provider = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+var BigNumber     = require('bignumber.js');
+var Web3            = require('web3');
 var contract        = require("truffle-contract");
 var path            = require('path');
-var MyContractJSON  = require(path.join(__dirname, '/../blockchain-Lottery-solidity/build/contracts/Lottery.json'));
+var MyContractJSON  = require('../blockchain-Lottery-solidity/build/contracts/Lottery.json');
 
 // function listen(){
 //
